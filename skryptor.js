03489077c0edd5d59e9a595ce2e58048cab4be56
Kanioyin <kamilpio326@gmail.com>
2023-12-11 15:22:50 +0100

    Initial commit

---
+++ skryptor.js
@@ -0,0 +1,8 @@
+function toggleStyle() {
+    var styleLink = document.getElementById("style-link");
+    if (styleLink.getAttribute("href") === "style.css") {
+        styleLink.setAttribute("href", "style2.css");
+    } else {
+        styleLink.setAttribute("href", "style.css");
+    }
+}
